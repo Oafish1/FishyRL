Alignment with original implementation, additional logging and video output, normalization fixes and optimal models, organization

fishyrl/actions.py
@@ -286,7 +286,7 @@ class DiscreteAction(Action):
 
         """
         # Create and sample from distribution
-        dist = torch.frl_distributions.OneHotCategoricalStraightThrough(
+        dist = torch.distributions.OneHotCategoricalStraightThrough(
                 logits=frl_distributions.uniform_mix(logits)[0])
         return dist.rsample(), dist
 

fishyrl/buffers.py
@@ -6,10 +6,9 @@ from typing import Any
 
 import numpy as np
 import torch
-from torch import nn
 
 
-class Buffer(nn.Module):
+class Buffer:
     """Class template for buffers."""
     # TODO: Maybe add more properties here, pending use cases
     @property

fishyrl/models.py
@@ -6,7 +6,8 @@ from typing import Any
 import torch
 import torch.nn as nn
 
-from . import actions, distributions
+from . import actions as frl_actions
+from . import distributions as frl_distributions
 
 
 class MLP(nn.Module):
@@ -57,7 +58,7 @@ class MLP(nn.Module):
 
 class MLPEncoder(nn.Module):
     """MLP encoder for processing vector observations."""
-    def __init__(self, input_dim: int, output_dim: int, num_blocks: int = 4, hidden_dim: int = 512) -> None:
+    def __init__(self, input_dim: int, output_dim: int, num_blocks: int = 4, hidden_dim: int = 512, use_symlog: bool = True) -> None:
         """Initialize the MLP encoder.
 
         :param input_dim: The dimension of the input vector observation.
@@ -68,9 +69,14 @@ class MLPEncoder(nn.Module):
         :type num_blocks: int
         :param hidden_dim: The dimension of the hidden layers. (Default: ``512``)
         :type hidden_dim: int
+        :param use_symlog: Whether to apply symlog transformation to the input. (Default: ``True``)
+        :type use_symlog: bool
 
         """
         super().__init__()
+
+        # Parameters
+        self._use_symlog = use_symlog
 
         # Create model
         self._model = MLP(
@@ -87,8 +93,8 @@ class MLPEncoder(nn.Module):
         :rtype: torch.Tensor
 
         """
-        return self._model(distributions.symlog(x))
-
+        x = frl_distributions.symlog(x) if self._use_symlog else x
+        return self._model(x)
 
 class MLPDecoder(nn.Module):
     """MLP decoder for reconstructing vector observations from latent representations."""
@@ -454,7 +460,7 @@ class RSSM(nn.Module):
         logits = logits.view(*logits.shape[:-1], -1, self._bins)
 
         # Mix with uniform distribution
-        logits, probs = distributions.uniform_mix(logits)
+        logits, probs = frl_distributions.uniform_mix(logits)
 
         # Sample
         # NOTE: SheepRL uses `torch.distributions.Independent`, but this is unneeded since we never
@@ -580,7 +586,7 @@ class Actor(nn.Module):
     def __init__(
         self,
         input_dim: int,
-        actions: list[actions.Action],
+        actions: list[frl_actions.Action],
         hidden_dim: int = 512
     ) -> None:
         """Initialize the actor network.

fishyrl/utilities.py
@@ -177,6 +177,49 @@ class ContainerModule(nn.Module):
             self.add_module(name, module)
 
 
+class Container:
+    """Container for containing multiple submodules and utilities, without torch integration."""
+    def __init__(self, **modules: Any) -> None:  # noqa: ANN401
+        """Initialize the ``Container``.
+
+        :param modules: The submodules and utilities to contain.
+        :type modules: dict[str, Any]
+
+        """
+        # Register the submodules and utilities
+        for name, module in modules.items():
+            setattr(self, name, module)
+
+    def state_dict(self) -> dict[str, Any]:
+        """Return the state of the container as a dictionary.
+
+        :return: A dictionary containing the state of the container.
+        :rtype: dict[str, Any]
+
+        """
+        state = {}
+        for name, module in self.__dict__.items():
+            if not hasattr(module, 'state_dict'):
+                raise ValueError(f'Module "{name}" does not have a state_dict method.')
+            state[name] = module.state_dict()
+
+        return state
+
+    def load_state_dict(self, state_dict: dict[str, Any]) -> None:
+        """Load the state of the container from a dictionary.
+
+        :param state_dict: The state dictionary.
+        :type state_dict: dict[str, Any]
+
+        """
+        for name, module in self.__dict__.items():
+            if not hasattr(module, 'load_state_dict'):
+                raise ValueError(f'Module "{name}" does not have a load_state_dict method.')
+            if name not in state_dict:
+                raise ValueError(f'Module "{name}" not found in state dictionary.')
+            module.load_state_dict(state_dict[name])
+
+
 def load_config(*paths: list[str], list_behavior: str = 'replace') -> DotDict:
     """Load and merge YAML configuration files into a single ``DotDict``, with priority given to earlier files.
 
@@ -233,7 +276,7 @@ def _merge_dotdicts(base: DotDict, new: DotDict, list_behavior: str = 'replace')
             base[k] = v
 
 
-def optional_flatten_cfg(func: callable = None, exceptions: list[str] = []) -> callable:
+def optional_flatten_cfg(func: callable = None, exceptions: list[str] = [], exclusions: list[str] = []) -> callable:
     """Decorator to optionallly flatten a config DotDict before passing it to the function.
 
     :param func: The function to decorate.
@@ -244,6 +287,8 @@ def optional_flatten_cfg(func: callable = None, exceptions: list[str] = []) -> c
         argument ``'model_default'``, where it would have otherwise passed
         ``'model_default_embedded'``, ``'model_default_blocks'``, etc.
     :type exceptions: list[str]
+    :param exclusions: A list of keys to exclude from being passed to the function. (Default: ``[]``)
+    :type exclusions: list[str]
 
     """
     @functools.wraps(func)
@@ -252,7 +297,7 @@ def optional_flatten_cfg(func: callable = None, exceptions: list[str] = []) -> c
         def wrapper(*args: list[Any], cfg: DotDict = None, **kwargs: dict[str, Any]) -> Any:  # noqa: ANN401
             # If cfg is provided, flatten and revise kwargs
             if cfg is not None:
-                new_kwargs = _flatten_dict(cfg, exceptions=exceptions)
+                new_kwargs = _flatten_dict(cfg, exceptions=exceptions, exclusions=exclusions)
                 new_kwargs.update(kwargs)  # Overwrite cfg kwargs with manual kwargs
                 kwargs = new_kwargs
             # Otherwise, just use normally
@@ -271,6 +316,7 @@ def optional_flatten_cfg(func: callable = None, exceptions: list[str] = []) -> c
 def _flatten_dict(
     base: DotDict | dict,
     exceptions: list[str] = [],
+    exclusions: list[str] = [],
     sep: str = '_',
     _key: str = '',
     _result: dict = {},
@@ -282,6 +328,8 @@ def _flatten_dict(
     :param exceptions: A list of keys to exclude from flattening. (Default: ``[]``)
         See the documentation for the ``optional_flatten_cfg`` decorator for more details.
     :type exceptions: list[str]
+    :param exclusions: A list of keys to exclude from being passed to the function. (Default: ``[]``)
+    :type exclusions: list[str]
     :param sep: The separator to use when concatenating keys. (Default: ``'_'``)
     :type sep: str
     :param _key: The current key prefix during recursion. (Default: ``''``)
@@ -298,9 +346,10 @@ def _flatten_dict(
         new_k = f'{_key}{sep}{k}' if _key else k
         # If value is a dictionary and new key is not in exceptions, recurse
         if isinstance(v, dict) and new_k not in exceptions:
-            _flatten_dict(v, exceptions=exceptions, sep=sep, _key=new_k, _result=_result)
-        # Otherwise, add to result
-        else: _result[new_k] = v
+            _flatten_dict(v, exceptions=exceptions, exclusions=exclusions, sep=sep, _key=new_k, _result=_result)
+        # Otherwise, add to result if new key is not in exclusions
+        elif new_k not in exclusions:
+            _result[new_k] = v
     return _result
 
 